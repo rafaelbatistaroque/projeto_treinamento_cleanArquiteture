e44ac3830f6c9d31f139fa0afc70dd79bd33d9ac
Rafael Batista <maranata_rafa@hotmail.com>
2020-11-23 00:58:46 -0400

    refactor: movido HttpPostClientSpy para novo arquivo separado

--- src/data/casos_de_uso/autenticacao/api-autenticacao.spec.ts
+++ src/data/casos_de_uso/autenticacao/api-autenticacao.spec.ts
@@ -1,15 +1,9 @@
-import { HttpPostClient } from "data/protocols/http/htt-post-client";
+
+import { HttpPostClientSpy } from "../../test/mock-http-client";
 import { ApiAutenticacao } from "./api-autenticacao";
 
 describe('ApiAutenticacao', () => {
   test('Deve chamar HttpPostClient com URL correta', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string;
-      async post(url: string): Promise<void> {
-        this.url = url;
-        return await Promise.resolve();
-      }
-    }
     const url = "qualquer_url";
     const httpPostClientSpy = new HttpPostClientSpy();
     const sut = new ApiAutenticacao(url, httpPostClientSpy);
