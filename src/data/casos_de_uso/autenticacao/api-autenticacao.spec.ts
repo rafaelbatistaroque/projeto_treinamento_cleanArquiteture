2d47deaf17598dd69c88979a171d8de4341d004a
Rafael Batista <maranata_rafa@hotmail.com>
2020-11-23 01:05:13 -0400

    refactor: movido criação do SUT para método factory

--- src/data/casos_de_uso/autenticacao/api-autenticacao.spec.ts
+++ src/data/casos_de_uso/autenticacao/api-autenticacao.spec.ts
@@ -1,12 +1,24 @@
 
 import { HttpPostClientSpy } from "../../test/mock-http-client";
 import { ApiAutenticacao } from "./api-autenticacao";
+type SutTypes = {
+  sut: ApiAutenticacao,
+  httpPostClientSpy: HttpPostClientSpy;
+};
+const criarSut = (url: string = "qualquer_url"): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy();
+  const sut = new ApiAutenticacao(url, httpPostClientSpy);
+  return {
+    sut,
+    httpPostClientSpy
+  };
+}
 
 describe('ApiAutenticacao', () => {
   test('Deve chamar HttpPostClient com URL correta', async () => {
-    const url = "qualquer_url";
-    const httpPostClientSpy = new HttpPostClientSpy();
-    const sut = new ApiAutenticacao(url, httpPostClientSpy);
+    const url = "outra_url";
+    const { sut, httpPostClientSpy } = criarSut(url);
+
     await sut.auth();
 
     expect(httpPostClientSpy.url).toBe(url);
